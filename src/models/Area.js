42c3ca8f848e922641557100d89d5310c017db9d
Philipp Moritzer <philipp.moritzer@yahoo.de>
2019-01-18 21:12:24 +0100

    alles fertig, hoffentlich

--- src/models/Area.js
+++
@@ -1,9 +0,0 @@
-class Area {
-  constructor(backgroundImage, name, type) {
-    this.backgroundImage = backgroundImage;
-    this.name = name;
-    this.type = type;
-  }
-
-  loadRandomActions(type) {}
-}
