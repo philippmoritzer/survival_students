74ba2706a95eb589fa4050ca5efb247448b192e7
Philipp Moritzer <philipp.moritzer@yahoo.de>
2019-01-18 12:26:19 +0100

    doku + präsi

---
+++ src/models/Action.js
@@ -0,0 +1,12 @@
+class Action {
+
+    constructor(name, desc, img, type, value, cost) {
+        this.name = name;
+        this.desc = desc;
+        this.img = img;
+        this.type = type;
+        this.value = value;
+        this.cost = cost;
+    }
+
+}
---
+++ src/models/Area.js
@@ -0,0 +1,9 @@
+class Area {
+  constructor(backgroundImage, name, type) {
+    this.backgroundImage = backgroundImage;
+    this.name = name;
+    this.type = type;
+  }
+
+  loadRandomActions(type) {}
+}
